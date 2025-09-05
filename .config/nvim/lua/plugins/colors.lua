939ec639728d286d97f21bf82dfbb9b568320ac5
Jack <78124447+JackDerksen@users.noreply.github.com>
2025-09-05 14:52:31 -0600

    Replace Sonokai colorscheme with 0x96f

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,11 +1,12 @@
 return {
-  "sainnhe/sonokai",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.g.sonokai_style = "default"
-    vim.g.sonokai_enable_italic = false
-    vim.g.sonokai_disable_italic_comment = true
-    vim.cmd.colorscheme("sonokai")
-  end,
+  {
+    dir = vim.fn.stdpath("config") .. "/lua/colorschemes/0x96f",
+    name = "0x96f",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Load the colorscheme
+      require("colorschemes.0x96f").setup()
+    end,
+  }
 }
